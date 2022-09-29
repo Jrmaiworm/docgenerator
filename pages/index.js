5cc125828ec1ae542ad9ab8418a91a89aaad0021
Jorge <Jrmaiworm@hotmail.com>
2022-09-29 20:00:07 -0300

    feat:input cpf

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,29 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Col , Card, Row, CardFooter,Text } from 'reactstrap'
+import { useState } from 'react';
+import { Col , Card, Row, CardFooter,Text, Input, Button } from 'reactstrap'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
+const [cpf, setcpf]=useState('');
+
+
+
+
+
   return (
    <Col style={{backgroundColor:'#bedfd4', width:'100vw', height: '100vh'}} >
-   <Row style={{backgroundColor:'#bedfd4', width:'100vw', height: '8vh'}} ></Row>
-   <Row style={{display:'flex', backgroundColor:'#f5f5f5', width:'100vw', height: '82vh'}} >
- 
+   <Row style={{backgroundColor:'#bedfd4', width:'100vw', height: '12vh'}} ></Row>
+   <Row style={{display:'flex', backgroundColor:'#f5f5f5', width:'100vw', height: '78vh'}} >
+    <Row style={{display:'flex',justifyContent:'center',alignItems:'center', backgroundColor:'#f4f4f4', width:'100vw'}}>
+    <Card >
+    <Input  style={{backgroundColor:'#fff', width:'280px', height:'50px', fontSize:'20px', marginRight:'10px'}} placeholder='Gerador de CPF' />
+    </Card>
+    <Button style={{backgroundColor:'#336a7a', width:'200px', height:'50px', fontSize:'15px'}} >GERAR CPF</Button>
+    </Row>
+    
+   
    </Row>
    <Row style={{display:'flex'}}>
    <CardFooter 
---
+++ src/geradorCpf.js
@@ -0,0 +1,29 @@
+import react from "react";
+
+const GeradorCPF =({cpf, mask})=> {
+   
+        while ((cpf % 2) != 0) {
+            var n1 = RandomUtils.getRandomInt(0, 9);
+            var n2 = RandomUtils.getRandomInt(0, 9);
+            var n3 = RandomUtils.getRandomInt(0, 9);
+            var n4 = RandomUtils.getRandomInt(0, 9);
+            var n5 = RandomUtils.getRandomInt(0, 9);
+            var n6 = RandomUtils.getRandomInt(0, 9);
+            var n7 = RandomUtils.getRandomInt(0, 9);
+            var n8 = RandomUtils.getRandomInt(0, 9);
+            var n9 = RandomUtils.getRandomInt(0, 9);
+
+            var dv1 = n9 * 2 + n8 * 3 + n7 * 4 + n6 * 5 + n5 * 6 + n4 * 7 + n3 * 8 + n2 * 9 + n1 * 10;
+            dv1 = RandomUtils.digitoVerificador(dv1);
+
+            var dv2 = dv1 * 2 + n9 * 3 + n8 * 4 + n7 * 5 + n6 * 6 + n5 * 7 + n4 * 8 + n3 * 9 + n2 * 10 + n1 * 11;
+            dv2 = RandomUtils.digitoVerificador(dv2);
+        }
+        return (mask) ?
+            `${n1}${n2}${n3}.${n4}${n5}${n6}.${n7}${n8}${n9}-${dv1}${dv2}` :
+            `${n1}${n2}${n3}${n4}${n5}${n6}${n7}${n8}${n9}${dv1}${dv2}`;
+    
+};
+export default GeradorCPF;
+
+
