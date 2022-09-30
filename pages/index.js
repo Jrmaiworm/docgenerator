5eaa76f63ba81a6ffefb94ec094ea6ef8ee3adfb
Jorge <Jrmaiworm@hotmail.com>
2022-09-30 13:21:11 -0300

    feat:gerando cpf ok

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,31 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import Image from 'next/image';
 import { useState } from 'react';
 import { Col , Card, Row, CardFooter,Text, Input, Button } from 'reactstrap'
 import styles from '../styles/Home.module.css'
+import logo from "../src/assets/logo5devs.png"
+import { generate } from 'gerador-validador-cpf'
 
 export default function Home() {
 
 const [cpf, setcpf]=useState('');
 
 
-
+const gerarcpf = async() =>{
+  await setcpf(generate({ format: true }));
+  console.log(cpf)
+}
 
 
   return (
    <Col style={{backgroundColor:'#bedfd4', width:'100vw', height: '100vh'}} >
-   <Row style={{backgroundColor:'#bedfd4', width:'90vw', height: '12vh', display:'flex', alignItems:'center', marginLeft:'10vw'}} ><img width={200} height={50} alt='logo' src='https://cdn.4devs.com.br/imagens/logo_4devs.png'/></Row>
+   <Row style={{backgroundColor:'#bedfd4', width:'90vw', height: '12vh', display:'flex', alignItems:'center', marginLeft:'10vw'}} ><Image  width={200} height={50} alt='logo' src={logo}/></Row>
    <Row style={{display:'flex', backgroundColor:'#f5f5f5', width:'100vw', height: '78vh'}} >
     <Row style={{display:'flex',justifyContent:'center',alignItems:'center', backgroundColor:'#f4f4f4', width:'100vw'}}>
     <Card >
-    <Input  style={{backgroundColor:'#fff', width:'280px', height:'50px', fontSize:'20px', marginRight:'10px', paddingLeft:'10px'}} placeholder='Gerador de CPF' />
+    <Input  style={{backgroundColor:'#fff', width:'280px', height:'50px', fontSize:'20px', marginRight:'10px', paddingLeft:'10px' ,color:'black'}} placeholder='Gerador de CPF' value={cpf}  />
     </Card>
-    <Button style={{backgroundColor:'#336a7a', width:'200px', height:'50px', fontSize:'15px'}} >GERAR CPF</Button>
+    <Button style={{backgroundColor:'#336a7a', width:'200px', height:'50px', fontSize:'15px'}} onClick={gerarcpf} >GERAR CPF</Button>
     </Row>
     
    
